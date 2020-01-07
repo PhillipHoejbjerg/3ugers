e0ec1956259ee13ba027bfa814ecb545e8874af5
PhillipHoejbjerg <58687573+PhillipHoejbjerg@users.noreply.github.com>
2020-01-07 13:57:38 +0100

    Update predicter.py

---
+++ predicter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 11:21:46 2020
+
+@author: Jacobsen
+"""
+
+
+#predicter
+
+
+
+
+
+import cv2
+import tensorflow as tf
+CATEGORIES = ["happy", "neutral", "sad"]
+def prepare(file):
+    IMG_SIZE = 50
+    img_array = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+model = tf.keras.models.load_model("CNN.model")
+image = 'smile.jpg' #your image path
+prediction = model.predict([image])
+prediction = list(prediction[0])
+print(CATEGORIES[prediction.index(max(prediction))])
+
+
+
+
+
+
