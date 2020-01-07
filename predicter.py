6dcf5394257e0784b84bfebba33a6299a3f7e1ff
flotbot <albertkjoller@users.noreply.github.com>
2020-01-07 14:10:58 +0100

    :)

--- predicter.py
+++ predicter.py
@@ -22,8 +22,8 @@ def prepare(file):
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 model = tf.keras.models.load_model("CNN.model")
-image = 'vh6to.jpg' #your image path
-prediction = model.predict(['vh6to.jpg'])
+image = 'smile.jpg' #your image path
+prediction = model.predict([image])
 prediction = list(prediction[0])
 print(CATEGORIES[prediction.index(max(prediction))])
 
