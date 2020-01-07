524e856256bc649826700d77fd0608cbc76da3c6
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2020-01-07 14:02:19 +0100

    Add files via upload

--- predicter.py
+++ predicter.py
@@ -22,8 +22,8 @@ def prepare(file):
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 model = tf.keras.models.load_model("CNN.model")
-image = 'smile.jpg' #your image path
-prediction = model.predict([image])
+image = 'vh6to.jpg' #your image path
+prediction = model.predict(['vh6to.jpg'])
 prediction = list(prediction[0])
 print(CATEGORIES[prediction.index(max(prediction))])
 
